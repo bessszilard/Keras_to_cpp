75d695ab12b0732de55b602e11f78f2a6da57767
bessszilard <bessszilard@gmail.com>
2019-09-19 10:52:59 +0200

    Add execution time calculation

---
+++ eclipse_proj/src/keras_to_cpp.cpp
@@ -0,0 +1,33 @@
+#include "keras_model.h"
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+using namespace keras;
+
+// Step 1
+// Dump keras model and input sample into text files
+// python dump_to_simple_cpp.py -a example/my_nn_arch.json -w example/my_nn_weights.h5 -o example/dumped.nnet
+// Step 2
+// Use text files in c++ example. To compile:
+// g++ keras_model.cc example_main.cc
+// To execute:
+// a.out
+
+int main() {
+	clock_t tStart = clock();
+	cout
+			<< "This is simple example with Keras neural network model loading into C++.\n"
+			<< "Keras model will be used in C++ for prediction only." << endl;
+
+	DataChunk *sample = new DataChunk2D();
+	sample->read_from_file("./example/sample_mnist.dat");
+	std::cout << sample->get_3d().size() << std::endl;
+	KerasModel m("./example/dumped.nnet", true);
+	m.compute_output(sample);
+	delete sample;
+	cout << endl << "Execution time: ";
+	cout << ((double) (clock() - tStart) * 1000 / CLOCKS_PER_SEC) << "[ms]" << endl;
+
+	return 0;
+}
