d211f804e455bceb76cf1edd09ca3be08212173b
bessszilard <bessszilard@gmail.com>
2019-09-23 19:53:19 +0200

    Refactor and add comments to NnLayer
    * Add comments for functions
    * Relu removed from dotprodWithRelu -> rename function to dotprod

--- mnist_toy/src/NnLayer.cpp
+++ mnist_toy/src/NnLayer.cpp
@@ -13,16 +13,20 @@
 
 // Dense ==========================================================================================
 // private ----------------------------------------------------------------------------------------
+/*
+ * Return the activation function result
+ * The input and the output is is vector_2d to get compatibility with Flatten's get_output function
+ */
 vector_2d Dense::activaction(const vector_2d &input) {
-	if(input[0].size() != 1)
-		throw std::invalid_argument("Activation couldn't completed, because this is not a column vector");
+	if (input[0].size() != 1)
+		throw std::invalid_argument(
+				"Activation couldn't completed, because this is not a column vector");
 	int orig_row = input.size();
 	vector_2d result(orig_row, vector_1d(1));
 
 	if (m_activation_type == "relu") {
-		for (int i = 0; i < orig_row; ++i) {
+		for (int i = 0; i < orig_row; ++i)
 			result[i][0] = input[i][0] < 0 ? 0 : input[i][0];
-		}
 		return result;
 	}
 	else if (m_activation_type == "softmax") {
@@ -46,11 +50,13 @@ Dense::Dense(const std::vector<std::vector<float> > &weights,
 				 const std::string &a_type) :
 	m_weights(weights), m_bias(bias), m_activation_type(a_type)
 {
-	//TODO throw exception if bias width is larger than 0
+	if(1 < m_bias.size())
+		throw std::invalid_argument("Invalid bias size");
 }
+
 Dense::~Dense() {}
 
-inline void dotprodWithRelu(vector_2d &m_weights, int col, const vector_2d &input, float m_bias, std::string act_type, float &output) {
+inline void dotprod(vector_2d &m_weights, int col, const vector_2d &input, float m_bias, std::string act_type, float &output) {
 	float sum = 0.0f;
 	for (size_t k = 0; k < m_weights.size(); ++k) {
 		sum += m_weights[k][col] * input[k][0];
@@ -58,7 +64,7 @@ inline void dotprodWithRelu(vector_2d &m_weights, int col, const vector_2d &inpu
 	sum += m_bias;
 	output = sum;
 }
-
+/*
 vector_2d Dense::get_output(const vector_2d &input) {
 	// TOOD check dimensions
 	vector_2d new_weights(m_bias[0].size(), vector_1d(1));
@@ -72,11 +78,34 @@ vector_2d Dense::get_output(const vector_2d &input) {
 //	for (size_t i = 0; i < m_weights[0].size(); ++i) {
 //		threads[i].join();
 //	}
+	return activaction(new_weights)s;
+}
+*/
+
+/*
+ * Calculates the output of the Dense layer.
+ * Formula: transpose(W) * X + transpose(B)
+ *		(n, 1) =  (n, m) * (m, 1) + (n, 1)
+ */
+vector_2d Dense::get_output(const vector_2d &input) {
+	if(m_weights.size() != input.size())
+		throw std::invalid_argument("Input size is not proper");
+
+	vector_2d new_weights(m_bias[0].size(), vector_1d(1));
+	for (size_t i = 0; i < m_weights[0].size(); ++i) {
+		dotprod(m_weights, i, input, m_bias[0][i], m_activation_type, new_weights[i][0]);
+	}
 	return activaction(new_weights);
 }
 
 // Flatten ========================================================================================
 // public ---------------------------------------------------------------------------------------
+
+/*
+ * Creates Resize input into column vector.
+ * input 	(n ,  m)
+ * output	(n*m, 1)
+ */
 vector_2d Flatten::get_output(const vector_2d &input) {
 	int orig_row = input.size();
 	int orig_col = input[0].size();
@@ -84,7 +113,7 @@ vector_2d Flatten::get_output(const vector_2d &input) {
 
 	for (int i = 0; i < orig_row; ++i) {
 		for (int j = 0; j < orig_col; ++j) {
-			int offset = (i * orig_col + j);
+			int offset = i * orig_col + j;
 			flat[offset][0] = input[i][j];
 		}
 	}
@@ -92,10 +121,20 @@ vector_2d Flatten::get_output(const vector_2d &input) {
 }
 
 // NeuralNetwork ==================================================================================
+/*
+ *	Add new layer to NeuralNetwork class.
+ *	Implemented layer types:
+ *		Flatten
+ *		Dense
+ */
 void NeuralNetwork::add_layer(NnLayer *layer) {
 	m_layers.push_back(layer);
 }
 
+/*
+ * Calculate the prediction for the given input
+ * get_output input and output is vector_2d for generalization
+ */
 vector_2d NeuralNetwork::predict(const vector_2d &input) {
 	vector_2d temp = input;
 	for(auto layer : m_layers) {
@@ -104,8 +143,11 @@ vector_2d NeuralNetwork::predict(const vector_2d &input) {
 	return temp;
 }
 
+/*
+ * Free the memory of the given layers.
+ */
 NeuralNetwork::~NeuralNetwork() {
-	for(int i = 0; i < (int)m_layers.size(); ++i) {
+	for(size_t i = 0; i < m_layers.size(); ++i) {
 		delete m_layers[i];
 	}
 }
