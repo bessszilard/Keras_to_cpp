05d33f180ecb3ec4624064ebd7b4e40949285324
bessszilard <bessszilard@gmail.com>
2019-09-24 11:18:54 +0200

    Image file location is readed from console

--- mnist_toy/src/keras_to_cpp_minst_toy.cpp
+++ mnist_toy/src/keras_to_cpp_minst_toy.cpp
@@ -11,6 +11,7 @@
 #include <math.h>
 #include <time.h>
 #include <algorithm>
+#include <stdexcept>
 #include "NnLayer.h"
 #include "dumped.h"
 #include "Utilities.h"
@@ -19,10 +20,16 @@ using std::cout;
 using std::endl;
 using std::ifstream;
 using std::vector;
+using std::string;
 
 int main(int argc, char** argv) {
 	Clocks clk;
 	vector_2d result;
+
+	if(argc < 2)
+		throw std::invalid_argument("Image path is needed");
+	string imgPath = argv[1];
+	cout << "Input: " << imgPath << endl;
 
 	int iterations = 50;
 	for(int i = 0; i < iterations; i++) {
@@ -35,7 +42,7 @@ int main(int argc, char** argv) {
 		nn.add_layer(new Dense(layer3DenseWeights, layer3DenseBias, layer3DenseActivation ));
 
 		clk.start_FileRead();
-		vector_2d input = Utilities::read_from_file("sample_mnist.dat");
+		vector_2d input = Utilities::read_from_file(imgPath);
 
 		clk.start_Prediction();
 		result = nn.predict(input);
@@ -43,14 +50,14 @@ int main(int argc, char** argv) {
 	}
 
 	int maxElementIndex = std::max_element(result.begin(), result.end()) - result.begin();
-	cout << "Prediction: " << maxElementIndex << endl;
+	cout << "Prediction: " << maxElementIndex << endl << endl;
 	for(size_t i = 0; i < result.size(); i++)
 		cout << result[i][0] << " ";
-	cout << endl << endl;
+	cout << endl;
 
 	cout << "Init:           " << clk.get_Init_average()          << " [ms]\t" << clk.get_Init_percent()		<< " %" << endl;
 	cout << "File read:      " << clk.get_FileRead_average()      << " [ms]\t" << clk.get_Float_percent()       << " %" << endl;
-	cout << "Calc out:       " << clk.get_Prediction_average()    << " [ms]\t" << clk.get_Prediction_percent()  << " %" << endl;
+	cout << "Prediction out: " << clk.get_Prediction_average()    << " [ms]\t" << clk.get_Prediction_percent()  << " %" << endl;
 	cout << "Execution time: " << clk.get_Execuition_average()    << " [ms]"   << endl;
 
 	return 0;
